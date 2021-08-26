75fc0962e22e371ecff08b18acc952d339bd2a29
Loic Gregoire <loic.gregoire@gtd.eu>
2021-08-26 17:08:06 +0200

    add projet into installed app.

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'apps.organization', 
     'apps.supplier',
+    'apps.documentation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes', 
