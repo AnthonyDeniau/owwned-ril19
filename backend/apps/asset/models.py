b86bb2da880c3de5eb76dc709176850c63bf776c
SodapriZ <58815280+SodapriZ@users.noreply.github.com>
2021-08-26 15:20:09 +0200

    Update models.py

--- backend/apps/asset/models.py
+++ backend/apps/asset/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Asset(models.Model):
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    picture = models.TextField()
+    cost = models.DecimalField()
+    supplier = models.CharField()
+
+    # team = models.ForeignKey
+
+    def __str__(self):
+        return self.name,self.description,self.picture,self.cost,self.supplier
