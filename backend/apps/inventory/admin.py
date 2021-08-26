b93225856d791b280948af9eea7653568554acdc
nicolas demoures <kowomati@gmail.com>
2021-08-26 15:59:16 +0200

    Update Asset + Inventory

--- backend/apps/inventory/admin.py
+++ backend/apps/inventory/admin.py
@@ -1,3 +1,5 @@
+from backend.apps.inventory.models import Inventory, InventoryItem, InventorySession
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Inventory, InventoryItem, InventorySession)
--- backend/apps/inventory/models.py
+++ backend/apps/inventory/models.py
@@ -1,3 +1,33 @@
+from enum import Enum
+from ..asset.models import Asset
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.deletion import CASCADE, RESTRICT
+
+class Status(Enum):
+    OK = "OK"
+    NON_FUNCTIONAL = "NON_FUNCTIONAL"
+    LOOSE = "LOOSE"
+
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
 
 # Create your models here.
+class InventorySession(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+class InventoryItem(models.Model):
+    inventory_session = models.ForeignKey(InventorySession)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    inventorist = models.ForeignKey(User, on_delete=models.SET_NULL)
+    date = models.DateTimeField()
+    comment = models.TextField()
+    status = models.CharField(max_length=255, choices=Status.choices())
+
+class Inventory(models.Model):
+    invetory_session = models.OneToOneField(InventorySession)
+    inventory_item = models.OneToOneField(InventoryItem)
