45f3a6195b9f91eb99c7896216a4c5f0e7f9981a
cesi2019 <quentin.laffitte@viacesi.fr>
2021-08-26 16:43:05 +0200

    Add Team and UserProfile

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.organization', 'django.contrib.admin', 'django.contrib.auth',
+    'apps.team', 'apps.userprofile', 'apps.organization', 'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles', 'graphene_django'
 ]
