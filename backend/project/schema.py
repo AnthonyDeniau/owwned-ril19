6480806bda592ae282cc0fcfde283e9e9da7518e
sebastien kircher <sebastien.kircher@gmail.com>
2021-08-27 09:32:10 +0200

    Query, Mutation UserProfile Model

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,13 +1,14 @@
 import graphene
 from apps.organization import schema as organization_schema
 from apps.team import schema as team_schema
+from apps.userprofile import schema as user_profile_schema
 
 
-class Query(organization_schema.Query,team_schema.Query, graphene.ObjectType):
+class Query(organization_schema.Query,team_schema.Query,user_profile_schema.Query, graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
 
 
-class Mutation(organization_schema.Mutation,team_schema.Mutation, graphene.ObjectType):
+class Mutation(organization_schema.Mutation,team_schema.Mutation,user_profile_schema.Mutation, graphene.ObjectType):
     pass
 
 
